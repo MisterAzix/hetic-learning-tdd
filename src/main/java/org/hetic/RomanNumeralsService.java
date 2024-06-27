681d94025af08c31ff32a7e3a6ac156d308f4551
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-06-27 16:02:08 +0200

    feat: pass second failing test ✨

--- src/main/java/org/hetic/RomanNumeralsService.java
+++ src/main/java/org/hetic/RomanNumeralsService.java
@@ -2,6 +2,10 @@ package org.hetic;
 
 public class RomanNumeralsService {
     public String processNumber(int number) {
+        if (number == 5) {
+            return "V";
+        }
+
         return "I";
     }
 }
