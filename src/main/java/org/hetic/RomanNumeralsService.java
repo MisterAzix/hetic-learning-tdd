14123803d0f8295c4ce034e2aeae98bfd269a3fb
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-06-27 22:17:56 +0200

    feat: pass 18th test

--- src/main/java/org/hetic/RomanNumeralsService.java
+++ src/main/java/org/hetic/RomanNumeralsService.java
@@ -10,6 +10,7 @@ public class RomanNumeralsService {
         romanNumerals.put(1000, "M");
         romanNumerals.put(900, "CM");
         romanNumerals.put(500, "D");
+        romanNumerals.put(400, "CD");
         romanNumerals.put(100, "C");
         romanNumerals.put(90, "XC");
         romanNumerals.put(50, "L");
